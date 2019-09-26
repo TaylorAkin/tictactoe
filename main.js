f67f68455f6ef35381e9271b8098be92fc83290e
TaylorAkin <taylorakin.ky@gmail.com>
2019-09-26 11:27:00 -0400

    finished UI

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ function creategame() {
     container.className = "container d-flex flex-column mt-1 align-self-center";
 
     var titlerow = document.createElement("div");
-    titlerow.setAttribute('class','row d-flex flex-grow-1');
+    titlerow.setAttribute('class','row mt-5 ml-5 mb-3 display-3');
+    titlerow.innerHTML = "TIC-TAC-TOE";
 
-    var row = document.createElement("div");
-    row.setAttribute('class','row d-flex flex-grow-1');
+    var boardrow = document.createElement("div");
+    boardrow.setAttribute('class','row d-flex flex-grow-1');
     
     
     //loop creating the 9 column divs
@@ -21,16 +22,50 @@ function creategame() {
         var tile = document.createElement("div");
         tile.setAttribute('class','col-4 flex-grow-1 border');
         tile.innerHTML= 'hello';
-        row.appendChild(tile);
+        boardrow.appendChild(tile);
         
         
         console.log(i);
         i++;
     }
+
+    var infocontainer = document.createElement("div");
+    infocontainer.id = "infocontainer";
+    infocontainer.className = "container-fluid d-flex flex-column mt-1 align-self-center";
+
+    var inforow = document.createElement("div");
+    inforow.className = "row d-flex flex-grow-1";
+
+    var infocol1 = document.createElement("div");
+    infocol1.className = "col h1 align-text-bottom";
+    infocol1.innerHTML = "Player " + "<span id='turntxt'></span> " + "'s turn"
+
+    var infocol2 = document.createElement("div");
+    infocol2.className = "col flex-grow-1";
+
+    var infocol3 = document.createElement("div");
+    infocol3.className = "col flex-grow-1";
+
+    var resetbtn = document.createElement("button");
+    resetbtn.className = "btn btn-primary btn-lg btn-block mt-3";
+    resetbtn.innerHTML = "RESET";
+
+
+    
+    
+
+
     
     container.appendChild(titlerow);
-    container.appendChild(row);
-    document.body.appendChild(container);
-    document.body.appendChild(title);
+    container.appendChild(boardrow);
+    infocol2.appendChild(resetbtn);
+    inforow.appendChild(infocol1);
+    inforow.appendChild(infocol2);
+    inforow.appendChild(infocol3);
+
+    infocontainer.appendChild(inforow);
+    document.getElementById('app').appendChild(container);
+    document.getElementById('app').appendChild(infocontainer);
+    
 
 }
